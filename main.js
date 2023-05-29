9241ce4b5d1ff0921761ebc275961c0488259dc4
Maria Isabel <mibalbuquerque@outlook.com>
2023-05-29 18:21:34 -0300

    animation fortune and functions update

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function funciona() {
   console.log("Funcionou");
 }
 
-// Constantes 
+// Constantes
 
 const screen1 = document.querySelector(".screen1");
 const screen2 = document.querySelector(".screen2");
@@ -19,31 +19,32 @@ const arraySentences = [
   "Surpreender e ser surpreendido é o segredo do amor.",
   "Todas as coisas são difíceis antes de se tornarem fáceis.",
   "O amor está sempre mais próximo do que você imagina.",
-  "Realize o óbvio, pense no improvável e conquiste o impossível."
+  "Realize o óbvio, pense no improvável e conquiste o impossível.",
 ];
-
-let number = Math.round(Math.random() * 8)
 
 // Eventos
 
-btnOpen.addEventListener('click', openBiscuit)
-btnNew.addEventListener('click', newBiscuit)
-
+btnOpen.addEventListener("click", openBiscuit);
+btnNew.addEventListener("click", newBiscuit);
 
 // Funcoes
 
 function openBiscuit() {
-
-toggleScreen()
-  screen2.querySelector("p").innerText = arraySentences[number]
+  newFortune();
+  toggleScreen();
 }
 
 function newBiscuit() {
-  toggleScreen()
-  number = Math.round(Math.random()*8)
+  toggleScreen();
+}
+
+function newFortune() {
+  let allFortune = arraySentences.length;
+  number = Math.round(Math.random() * allFortune);
+  screen2.querySelector("p").innerText = arraySentences[number];
 }
 
 function toggleScreen() {
-  screen1.classList.toggle("hide")
-  screen2.classList.toggle("hide") 
+  screen1.classList.toggle("hide");
+  screen2.classList.toggle("hide");
 }
